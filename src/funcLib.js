888115349bab9ea3de1f627c8534bcd006889295
Your Name <you@example.com>
2020-09-15 10:48:41 +0200

    a little cleanup

--- src/funcLib.js
+++ src/funcLib.js
@@ -1,10 +1,3 @@
-//Funktion til at oprette elementer
-/*function createEl (noOfEl, tag, arr){
-    for(i = 0; i < noOfEl; i++){
-        arr = [document.createElement(tag)];
-    }
-}*/
-
 //Funktion til at udeskrive array 
 function outputArray(theArray){
     for(i = 0; i < theArray.length; i++){
